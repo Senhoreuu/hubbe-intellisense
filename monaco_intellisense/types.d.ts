0172de16b4ffe38745c31202fbedf39abb2b8738
Luiz Guilherme <luizsimonetti888@gmail.com>
2024-01-14 04:10:40 -0300

    To use in Monaco Editor

--- monaco_intellisense/types.d.ts
+++ monaco_intellisense/types.d.ts
@@ -1215,9 +1215,10 @@ declare class Commands {
     /**
      * @description Registra um comando para ser executado.
      * @param commandName - Nome do comando a ser registrado.
+     * @param needStart - Se o comando precisa iniciar na frase.
      * @param callback - Função a ser executada quando o comando for chamado.
      */
-    static register(commandName: string, callback: () => void): void;
+    static register(commandName: string, needStart: boolean, callback: () => void): void;
 
     /**
      * @description Remove um comando registrado.
